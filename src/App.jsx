c87e2cd8aedb3d574353d8dba0b0bf3a2304cc66
shubham karad <shubham.karad12@gmail.com>
2025-09-06 16:40:49 +0530

    test-cases resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
 import { useState } from 'react';
 import '../src/App.css'
 
+const App = () => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [fullName, setFullName] = useState('')
 
-const App=()=>{
-
-   const [firstName,setFirstName]=useState('')
-   const [lastName,setLastName]=useState('')
-   const [fullName,setFullName]=useState('')
-   const submitData=(e)=>{
+  const submitData = (e) => {
     e.preventDefault()
- 
     setFullName(`${firstName} ${lastName}`)
+  }
 
-   }
   return (
-   
     <>
-    <div className='formContainer'>
-      <form onSubmit={(e)=>submitData(e)}>
-      <div className='inputGroup'>
-        <label htmlFor="firstName">First Name</label>&nbsp;
-        <input type="text" name="firstName" id=""  onChange={(e)=>setFirstName(e.target.value)} required />
+      <div className='formContainer'>
+        <form onSubmit={submitData}>
+          <div className='inputGroup'>
+            <label htmlFor="firstName">First Name</label>&nbsp;
+            <input 
+              type="text" 
+              name="firstName" 
+              onChange={(e) => setFirstName(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className='inputGroup'>
+            <label htmlFor="lastName">Last Name</label>&nbsp;
+            <input 
+              type="text" 
+              name="lastName" 
+              onChange={(e) => setLastName(e.target.value)} 
+              required 
+            />
+          </div>
+          <div>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+
+        {fullName 
+          ? <span>Full Name: {fullName}</span> 
+          : <span>Full Name Display</span>
+        }
       </div>
-       <div className='inputGroup'>
-        <label htmlFor="lastName">Last Name</label>&nbsp;
-        <input type="text" name="lastName" id="" onChange={(e)=>setLastName(e.target.value)} required />
-      </div>
-      <div>
-        <button type='submit'>Submit</button>
-      </div>
-    </form>
-    {fullName? <span>Full Name:{fullName}</span>:''
-      
-    }
-   
-    </div>
-    
     </>
   )
 }
